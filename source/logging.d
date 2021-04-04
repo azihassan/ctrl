14349d67df7b04a06351c796fbe25a30a56188b9
Azi Hassan <azi.hassan@live.fr>
2021-04-04 20:43:32 +0100

    [fix/ISSUE-4] Remove temp files after test finishes

--- source/logging.d
+++ source/logging.d
@@ -30,14 +30,17 @@ struct Logger
 
 unittest
 {
-    import std.file : readText;
+    import std.file : readText, remove;
     import std.stdio : writeln;
 
     writeln("Test that the logger writes to the output stream");
     auto logger = Logger(1, File("tmp", "w"));
+    scope(exit) "tmp".remove();
     logger("foo ", 1, " bar : ", true);
 
     assert("tmp".readText == "foo 1 bar : true\n");
+    logger("a ", false, " b: ", true);
+    assert("tmp".readText == "foo 1 bar : true\na false b: true\n");
     writeln("OK");
     writeln("");
 }
